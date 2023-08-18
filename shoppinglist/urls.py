cbf31d2fd8a521eead45a2723056196f9a935803
Kamil Wilczek <kamilwilczek@wp.pl>
2023-08-18 10:41:47 +0200

    shoppinglist urls extraction

--- shoppinglist/urls.py
+++ shoppinglist/urls.py
@@ -1,6 +1,29 @@
 from django.urls import path
-from . import views
+
+from .views import (
+    shopping_list_list_view,
+    shopping_list_create_view,
+    shopping_list_update_view,
+    shopping_list_delete_view,
+    item_update_view,
+    item_create_view,
+)
+
+app_name = "shopping_list"
 
 urlpatterns = [
-    path("", views.get_routes, name="routes"),
+    path("", shopping_list_list_view, name="list"),
+    path("create-update/", shopping_list_create_view, name="create"),
+    path("<int:id>/edit/", shopping_list_update_view, name="update"),
+    path("<int:id>/delete/", shopping_list_delete_view, name="delete"),
+    path(
+        "<int:parent_id>/item/",
+        item_create_view,
+        name="item-create",
+    ),
+    path(
+        "<int:parent_id>/item/<int:id>/",
+        item_update_view,
+        name="item-update",
+    ),
 ]
