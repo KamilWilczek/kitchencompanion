55d55db65b76dcf53cf26654392bbc663607b266
Kamil Wilczek <kamilwilczek@wp.pl>
2023-08-18 10:43:14 +0200

    shoppinglist admin extraction

--- shoppinglist/admin.py
+++ shoppinglist/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, ShoppingList
+
+
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 0
+
+
+class ShoppingListAdmin(admin.ModelAdmin):
+    inlines = [ItemInline]
+    readonly_fields = ["created", "updated"]
+
+
+admin.site.register(ShoppingList, ShoppingListAdmin)
