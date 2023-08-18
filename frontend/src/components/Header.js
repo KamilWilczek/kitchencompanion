cc9b70eda3ee020e83a15546b5652974f7d7d4b8
Kamil Wilczek <kamilwilczek@wp.pl>
2023-08-18 15:06:18 +0200

    App name in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='app-header'>
-        <h1>My shopping lists</h1>
+        <h1>Kitchen Companion</h1>
     </div>
   )
 }
