7298efa4eb1a18978a482b5c802694841dba982c
Kamil Wilczek <kamilwilczek@wp.pl>
2023-08-28 21:48:29 +0200

    mixins.py typing

--- shoppinglist/mixins.py
+++ shoppinglist/mixins.py
@@ -1,3 +1,5 @@
+from typing import Optional, Any, Dict
+
 from rest_framework import exceptions
 
 from .models import ShoppingList
@@ -8,8 +10,14 @@ class ShoppingItemMixin:
     Mixin to centralize the logic of retrieving shopping list from parent_pk.
     """
 
-    def get_shopping_list(self):
-        parent_pk = self.kwargs.get("parent_pk")
+    # Assuming that self.kwargs comes from Django's class-based views or DRF
+    kwargs: Dict[str, Any]
+
+    def get_shopping_list(self) -> ShoppingList:
+        parent_pk: Optional[str] = self.kwargs.get("parent_pk")
+        if parent_pk is None:
+            raise exceptions.ValidationError("parent_pk not provided in kwargs.")
+
         try:
             return ShoppingList.objects.get(pk=parent_pk)
         except ShoppingList.DoesNotExist:
