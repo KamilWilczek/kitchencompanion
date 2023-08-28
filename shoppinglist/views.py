1277e60313536060d7d4480fd655f0bd8856751e
Kamil Wilczek <kamilwilczek@wp.pl>
2023-08-28 20:32:54 +0200

    class based view

--- shoppinglist/views.py
+++ shoppinglist/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import generics, status
 from .serializers import ShoppingListSerializer, ItemSerializer
 
@@ -12,71 +10,20 @@ def error_response(detail, status_code):
     return Response({"detail": detail}, status=status_code)
 
 
-# # List of shopping lists
-# @api_view(["GET"])
-# def shoppinglist_list_view(request):
-#     qs = ShoppingList.objects.annotate(items_count=Count("items"))
-#     serialized_qs = ShoppingListSerializer(qs, many=True)
-#     return Response(serialized_qs.data, status=status.HTTP_200_OK)
-
-
 class ShoppingListView(generics.ListAPIView):
     queryset = ShoppingList.objects.annotate(items_count=Count("items"))
     serializer_class = ShoppingListSerializer
 
 
-# Creating shopping list
-# @api_view(["POST"])
-# def shoppinglist_create_view(request):
-#     if request.method == "POST":
-#         serializer = ShoppingListSerializer(data=request.data)
-
-
-#         if serializer.is_valid():
-#             obj = serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class ShoppingListCreateView(generics.CreateAPIView):
     serializer_class = ShoppingListSerializer
 
 
-# Editing of shopping list
-# @api_view(["GET", "PUT"])
-# def shoppinglist_update_view(request, id=None):
-#     obj = get_object_or_404(ShoppingList.objects.prefetch_related("items"), id=id)
-
-#     # For GET request, return the current state of the ShoppingList
-#     if request.method == "GET":
-#         serializer = ShoppingListSerializer(obj)
-#         return Response(serializer.data)
-
-
-#     # For PUT request, update the ShoppingList
-#     if request.method == "PUT":
-#         serializer = ShoppingListSerializer(obj, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class ShoppingListDetailUpdateView(generics.RetrieveUpdateAPIView):
     queryset = ShoppingList.objects.prefetch_related("items")
     serializer_class = ShoppingListSerializer
 
 
-# Deleting shopping lists
-# @api_view(["DELETE"])
-# def shoppinglist_delete_view(request, id=None):
-#     try:
-#         obj = ShoppingList.objects.get(id=id)
-#     except ShoppingList.DoesNotExist:
-#         return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-
-
-#     if request.method == "DELETE":
-#         obj.delete()
-#         return Response(
-#             {"detail": "Deleted successfully."}, status=status.HTTP_204_NO_CONTENT
-#         )
 class ShoppingListDeleteView(generics.DestroyAPIView):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
@@ -89,90 +36,32 @@ class ShoppingListDeleteView(generics.DestroyAPIView):
         )
 
 
-# Edit item
-# @api_view(["GET", "PUT"])
-# def item_update_view(request, parent_id=None, id=None):
-#     item = get_object_or_404(Item, shoppinglist__id=parent_id, id=id)
-
-#     # For GET request, return the current state of the Item
-#     if request.method == "GET":
-#         serializer = ItemSerializer(item)
-#         return Response(serializer.data)
-
-
-#     # For PUT request, update the Item
-#     elif request.method == "PUT":
-#         serializer = ItemSerializer(item, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class ItemUpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = ItemSerializer
 
     def get_queryset(self):
         parent_pk = self.kwargs["parent_pk"]
-        return Item.objects.filter(shoppinglist__id=parent_pk)
+        return Item.objects.filter(shoppinglist__pk=parent_pk)
 
 
-# Creating item
-# @api_view(["POST"])
-# def item_create_view(request, parent_id=None):
-#     try:
-#         parent_obj = ShoppingList.objects.get(id=parent_id)
-#     except ShoppingList.DoesNotExist:
-#         return Response(
-#             {"detail": "ShoppingList not found."}, status=status.HTTP_404_NOT_FOUND
-#         )
-
-
-#     serializer = ItemSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save(shoppinglist=parent_obj)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 class ItemCreateView(generics.CreateAPIView):
     serializer_class = ItemSerializer
 
     def perform_create(self, serializer):
         parent_pk = self.kwargs.get("parent_pk")
         try:
-            shopping_list = ShoppingList.objects.get(id=parent_pk)
+            shopping_list = ShoppingList.objects.get(pk=parent_pk)
             serializer.save(shoppinglist=shopping_list)
         except ShoppingList.DoesNotExist:
             raise error_response("ShoppingList not found.", status.HTTP_404_NOT_FOUND)
 
 
-# Deleting item
-# @api_view(["DELETE"])
-# def item_delete_view(request, parent_id=None, id=None):
-#     try:
-#         # Fetch the specific ShoppingList based on parent_id
-#         shopping_list = ShoppingList.objects.get(id=parent_id)
-
-#         # Try to get the item within that shopping list with the provided id
-#         item = shopping_list.items.get(
-#             id=id
-#         )  # Assuming "items" is the related name for the Item model
-#         item.delete()
-
-#         return Response(
-#             {"detail": "Item deleted successfully."}, status=status.HTTP_200_OK
-#         )
-
-
-#     except ShoppingList.DoesNotExist:
-#         return Response(
-#             {"detail": "Shopping List not found."}, status=status.HTTP_404_NOT_FOUND
-#         )
-#     except Item.DoesNotExist:
-#         return Response({"detail": "Item not found."}, status=status.HTTP_404_NOT_FOUND)
 class ItemDeleteView(generics.DestroyAPIView):
     serializer_class = ItemSerializer
 
     def get_queryset(self):
         parent_pk = self.kwargs["parent_pk"]
-        return Item.objects.filter(shoppinglist__id=parent_pk)
+        return Item.objects.filter(shoppinglist__pk=parent_pk)
 
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
