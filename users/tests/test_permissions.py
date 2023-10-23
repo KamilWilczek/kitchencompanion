7a345c9da06e744a3a86d5cd83e4192f51cc2a5d
Kamil Wilczek <kamilwilczek@wp.pl>
2023-10-23 21:52:15 +0200

    authenticated user not have access to login-register

--- users/tests/test_permissions.py
+++ users/tests/test_permissions.py
@@ -2,6 +2,8 @@ import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from users.models import CustomUser
+
 
 @pytest.mark.django_db
 class TestIsAuthenticated:
@@ -14,7 +16,6 @@ class TestIsAuthenticated:
     def test_protected_endpoint_with_invalid_token(
         self, authenticated_api_client: APIClient
     ):
-        # Overwrite the token with an invalid one
         authenticated_api_client.credentials(
             HTTP_AUTHORIZATION="Token " + "invalid_token_string"
         )
@@ -22,3 +23,33 @@ class TestIsAuthenticated:
         response = authenticated_api_client.get("/auth/token/logout/")
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_login_while_authenticated(
+        self, authenticated_api_client: APIClient, authenticated_user: CustomUser
+    ):
+        response = authenticated_api_client.post(
+            "/auth/token/login/",
+            {
+                "email": authenticated_user.email,
+                "password": authenticated_user.password,
+            },
+        )
+
+        assert (
+            response.status_code == status.HTTP_403_FORBIDDEN
+        ), "Authenticated user should not be able to access login endpoint"
+
+    def test_register_while_authenticated(
+        self, authenticated_api_client: APIClient, authenticated_user: CustomUser
+    ):
+        response = authenticated_api_client.post(
+            "/auth/users/",
+            {
+                "email": authenticated_user.email,
+                "password": authenticated_user.password,
+            },
+        )
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Authenticated user should not be able to access register endpoint"
