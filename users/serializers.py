cbe803ee7999380efc7c20fbe5ff8284cd4ec613
Kamil Wilczek <kamilwilczek@wp.pl>
2023-10-26 21:29:38 +0200

    more users tests

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator, validate_email
 from djoser.serializers import TokenCreateSerializer as DjoserTokenCreateSerializer
+from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
 
 
@@ -11,17 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "email", "first_name", "last_name", "date_joined")
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
+class RegisterSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
         fields = ("id", "email", "password")
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
-            validated_data["email"], validated_data["password"]
-        )
-        return user
+        return super().create(validated_data)
 
 
 class LoginSerializer(DjoserTokenCreateSerializer):
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,8 +1,7 @@
 import datetime
-import json
+import re
 
 import pytest
-from axes.models import AccessAttempt, AccessLog
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.core import mail
 from django.core.cache import cache
@@ -23,7 +22,7 @@ class TestRegister:
         url = "/auth/users/"
         data: dict[str, str] = {
             "email": "notregistereduser@example.com",
-            "password": "notregistereduserpassword",
+            "password": "asdqfgfregsfsafrfgwe",
         }
         response = not_authenticated_api_client.post(url, data)
 
@@ -49,7 +48,7 @@ class TestRegister:
 
     def test_registration_sends_email(self, not_authenticated_api_client: APIClient):
         url = "/auth/users/"
-        data = {"email": "newuser@example.com", "password": "newpassword"}
+        data = {"email": "newuser@example.com", "password": "asdqfgfregsfsafrfgwe"}
 
         response = not_authenticated_api_client.post(url, data=data)
 
@@ -74,13 +73,9 @@ class TestRegister:
         ), "Should not allow extremely long email"
 
         assert "email" in response.data, "Response should contain email error"
-        assert "password" in response.data, "Response should contain password error"
         assert response.data["email"] == [
             "Enter a valid email address."
         ], "Unexpected email error message"
-        assert response.data["password"] == [
-            "Ensure this field has no more than 128 characters."
-        ], "Unexpected password error message"
 
     def test_register_with_special_characters(
         self, not_authenticated_api_client: APIClient
@@ -101,6 +96,104 @@ class TestRegister:
         user_exists = CustomUser.objects.filter(email=special_email).exists()
         assert user_exists, "User with special characters in email should be created"
 
+    def test_register_with_invalid_email_format(
+        self, not_authenticated_api_client: APIClient
+    ):
+        invalid_email = "invalid-email-format"
+        password = "validpassword123"
+
+        response = not_authenticated_api_client.post(
+            "/auth/users/", {"email": invalid_email, "password": password}
+        )
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should not allow registration with an invalid email format"
+        assert (
+            "email" in response.data
+        ), "Response should contain an error for the email field"
+        assert (
+            "Enter a valid email address." in response.data["email"]
+        ), "Expected an error message indicating the email format is invalid"
+
+    def test_register_with_very_short_password(
+        self, not_authenticated_api_client: APIClient
+    ):
+        email = "user@example.com"
+        short_password = "pwd"
+
+        response = not_authenticated_api_client.post(
+            "/auth/users/", {"email": email, "password": short_password}
+        )
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should not allow registration with a very short password"
+        assert (
+            "password" in response.data
+        ), "Response should contain an error for the password field"
+        assert (
+            response.data["password"][0].code == "password_too_short"
+        ), "Expected an error code indicating the password is too short"
+        assert (
+            "This password is too short." in response.data["password"][0]
+        ), "Expected an error message indicating the password is too short"
+
+    def test_register_with_simple_password(
+        self, not_authenticated_api_client: APIClient
+    ):
+        simple_password = "password"
+        user_data = {
+            "email": "user@example.com",
+            "password": simple_password,
+        }
+
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should not allow simple password"
+        assert (
+            "password" in response.data
+        ), "Response should contain an error for the password field"
+        assert any(
+            "common" in error.lower() for error in response.data["password"]
+        ), "Expected an error message indicating the password is too common"
+
+    def test_register_without_email(self, not_authenticated_api_client: APIClient):
+        user_data = {
+            "password": "SecurePassword123!",
+        }
+
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should require an email"
+        assert (
+            "email" in response.data
+        ), "Response should contain an error for the missing email field"
+        assert response.data["email"] == [
+            "This field is required."
+        ], "Expected an error message indicating the email is required"
+
+    def test_register_without_password(self, not_authenticated_api_client: APIClient):
+        user_data = {
+            "email": "user@example.com",
+        }
+
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+
+        assert (
+            response.status_code == status.HTTP_400_BAD_REQUEST
+        ), "Should require a password"
+        assert (
+            "password" in response.data
+        ), "Response should contain an error for the missing password field"
+        assert response.data["password"] == [
+            "This field is required."
+        ], "Expected an error message indicating the password is required"
+
 
 @pytest.mark.django_db
 class TestLogin:
@@ -110,6 +203,7 @@ class TestLogin:
         external_user: CustomUser,
         external_user_password: str,
     ):
+        cache.clear()
         url = "/auth/token/login/"
         data: dict[str, str] = {
             "email": external_user.email,
@@ -131,6 +225,7 @@ class TestLogin:
         not_authenticated_api_client: APIClient,
         external_user: CustomUser,
     ):
+        cache.clear()
         url = "/auth/token/login/"
 
         incorrect_email_data: dict[str, str] = {
@@ -152,6 +247,7 @@ class TestLogin:
         not_authenticated_api_client: APIClient,
         external_user: CustomUser,
     ):
+        cache.clear()
         url = "/auth/token/login/"
 
         incorrect_password_data: dict[str, str] = {
@@ -168,6 +264,7 @@ class TestLogin:
         )
 
     def test_login_with_long_email(self, not_authenticated_api_client: APIClient):
+        cache.clear()
         long_email = "user@" + "a" * 240 + ".com"
         long_password = "P@ssw0rd" + "a" * 245
 
@@ -191,6 +288,7 @@ class TestLogin:
     def test_login_with_special_characters(
         self, not_authenticated_api_client: APIClient
     ):
+        cache.clear()
         special_email = "special.user+test@example.com"
         special_password = "P@ss!w#rd$%"
 
@@ -211,6 +309,125 @@ class TestLogin:
         assert Token.objects.filter(
             user=user
         ).exists(), "Token should be created for user with special characters in email"
+
+    def test_login_without_activation(self, not_authenticated_api_client: APIClient):
+        cache.clear()
+        user_data = {
+            "email": "test@example.com",
+            "password": "securepassword123",
+        }
+
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        user = CustomUser.objects.get(email=user_data["email"])
+        assert (
+            not user.is_active
+        ), "User should not be active immediately after registration"
+
+        response = not_authenticated_api_client.post("/auth/token/login/", user_data)
+        assert (
+            response.status_code == status.HTTP_403_FORBIDDEN
+        ), "User should not be able to log in without activation"
+
+    def test_activation_and_login(self, not_authenticated_api_client: APIClient):
+        cache.clear()
+        user_data = {
+            "email": "test@example.com",
+            "password": "securepassword123",
+        }
+
+        # Step 1: Register a new user
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        # Confirm the user is not active
+        user = CustomUser.objects.get(email=user_data["email"])
+        assert (
+            not user.is_active
+        ), "User should not be active immediately after registration"
+
+        # Step 2: Retrieve activation token/link from email
+        assert len(mail.outbox) == 1, "An activation email should be sent"
+        activation_email = mail.outbox[0]
+        activation_link_pattern = re.compile(r"/activate/(.*?)/(.*?)/")
+        activation_link_match = activation_link_pattern.search(activation_email.body)
+        assert (
+            activation_link_match
+        ), "Activation token and UID should be present in the email"
+
+        uid, token = activation_link_match.groups()
+        activation_data = {"uid": uid, "token": token}
+        response = not_authenticated_api_client.post(
+            "/auth/users/activation/", activation_data
+        )
+        assert response.status_code == 204, "Activation should be successful"
+
+        # Confirm the user is now active
+        user.refresh_from_db()
+        assert (
+            user.is_active
+        ), "User should be active after clicking the activation link"
+
+        # Step 3: Log in with the activated user
+        response = not_authenticated_api_client.post("/auth/token/login/", user_data)
+        assert (
+            response.status_code == status.HTTP_200_OK
+        ), "Activated user should be able to log in"
+        assert (
+            "auth_token" in response.data
+        ), "Login response should contain the authentication token"
+
+    def test_activation_link_single_use(self, not_authenticated_api_client: APIClient):
+        cache.clear()
+        user_data = {
+            "email": "test@example.com",
+            "password": "securepassword123",
+        }
+
+        # Step 1: Register a new user
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        # Confirm the user is not active
+        user = CustomUser.objects.get(email=user_data["email"])
+        assert (
+            not user.is_active
+        ), "User should not be active immediately after registration"
+
+        # Step 2: Retrieve activation token/link from email
+        assert len(mail.outbox) == 1, "An activation email should be sent"
+        activation_email = mail.outbox[0]
+        activation_link_pattern = re.compile(r"/activate/(.*?)/(.*?)/")
+        activation_link_match = activation_link_pattern.search(activation_email.body)
+        assert (
+            activation_link_match
+        ), "Activation token and UID should be present in the email"
+
+        uid, token = activation_link_match.groups()
+        activation_url = f"/activate/{uid}/{token}/"
+
+        # Step 3: Activate the user
+        uid, token = activation_link_match.groups()
+        activation_data = {"uid": uid, "token": token}
+        response = not_authenticated_api_client.post(
+            "/auth/users/activation/", activation_data
+        )
+        assert (
+            response.status_code == status.HTTP_204_NO_CONTENT
+        ), "Activation should be successful"
+
+        # Confirm the user is now active
+        user.refresh_from_db()
+        assert user.is_active, "User should be active after activation"
+
+        # Step 4: Attempt to use the activation link/token again
+        response = not_authenticated_api_client.post(
+            "/auth/users/activation/", activation_data
+        )
+        assert (
+            response.status_code == status.HTTP_403_FORBIDDEN
+        ), "Second activation attempt should fail"
 
 
 @pytest.mark.django_db
@@ -291,6 +508,7 @@ class TestPasswordReset:
     def test_request_password_reset(
         self, not_authenticated_api_client: APIClient, external_user: CustomUser
     ):
+        cache.clear()
         url = "/auth/users/reset_password/"
         data = {"email": external_user.email}
 
@@ -307,6 +525,7 @@ class TestPasswordReset:
     def test_reset_password(
         self, not_authenticated_api_client: APIClient, external_user: CustomUser
     ):
+        cache.clear()
         token_generator = PasswordResetTokenGenerator()
         reset_token = token_generator.make_token(external_user)
 
@@ -325,6 +544,7 @@ class TestPasswordReset:
     def test_login_with_new_password(
         self, not_authenticated_api_client: APIClient, external_user: CustomUser
     ):
+        cache.clear()
         external_user.set_password("newpassword123")
         external_user.save()
 
@@ -364,8 +584,6 @@ class TestSecurity:
         self, not_authenticated_api_client: APIClient
     ):
         cache.clear()
-        AccessAttempt.objects.all().delete()
-        AccessLog.objects.all().delete()
         wrong_credentials = {
             "email": "testuser@example.com",
             "password": "wrongpassword",
@@ -398,8 +616,6 @@ class TestSecurity:
         self, authenticated_api_client: APIClient, authenticated_user: CustomUser
     ):
         cache.clear()
-        AccessAttempt.objects.all().delete()
-        AccessLog.objects.all().delete()
         rate_limit = 10
         rate_limit_exceed = rate_limit + 1
         cooldown_period = datetime.timedelta(seconds=60)
@@ -425,3 +641,50 @@ class TestSecurity:
             )
 
             assert response.status_code == status.HTTP_200_OK, response.content
+
+    def test_rate_limit_registration_same_user(
+        self, not_authenticated_api_client: APIClient
+    ):
+        cache.clear()
+        user_data = {
+            "email": "testuser@example.com",
+            "password": "securepassword123",
+        }
+
+        # Attempt to register the same user multiple times
+        for _ in range(5):
+            response = not_authenticated_api_client.post("/auth/users/", user_data)
+            assert response.status_code in [
+                status.HTTP_201_CREATED,
+                status.HTTP_400_BAD_REQUEST,
+            ]
+
+        # One more time to check if rate limiting kicks in
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+        assert (
+            response.status_code != status.HTTP_429_TOO_MANY_REQUESTS
+        ), "Rate limiting should be configured for registration attempts"
+
+    def test_rate_limit_registration_distinct_users(
+        self, not_authenticated_api_client: APIClient
+    ):
+        cache.clear()
+        for i in range(5):
+            user_data = {
+                "email": f"testuser{i}@example.com",
+                "password": "securepassword123",
+            }
+            response = not_authenticated_api_client.post("/auth/users/", user_data)
+            assert (
+                response.status_code == status.HTTP_201_CREATED
+            ), "Should allow registration of distinct users"
+
+        # One more time to check if rate limiting kicks in
+        user_data = {
+            "email": "testuser6@example.com",
+            "password": "securepassword123",
+        }
+        response = not_authenticated_api_client.post("/auth/users/", user_data)
+        assert (
+            response.status_code != status.HTTP_429_TOO_MANY_REQUESTS
+        ), "Rate limiting should be configured for rapid user registrations"
